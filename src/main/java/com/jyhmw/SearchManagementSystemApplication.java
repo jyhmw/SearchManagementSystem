e97a87037e1ae772ad426e0af770e82cca56837b
jyhmw <1693746418@qq.com>
2025-02-12 17:22:24 +0800

    项目初始化，后续会添加依赖以及代码

---
+++ src/main/java/com/jyhmw/SearchManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jyhmw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jyhmw/SearchManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyhmw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
